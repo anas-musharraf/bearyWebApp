81b073b6a16bdab808cb4a913ec28ea7a0132ab8
anas-musharraf <anas.musharraf1207@gmail.com>
2020-03-09 12:22:57 -0400

    Navbar complete

--- bearyapp/src/components/home/Home.js
+++ bearyapp/src/components/home/Home.js
@@ -5,21 +5,17 @@ import '../../styles/Home.css';
 function Home() {
   return (
     <div>
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} class="header">
+    <div className={"header"}>
       <h1>Beary</h1>
-      <Link className="home__button-link center" to={"./../Home"}>
-        <h2>Home</h2>
-      </Link>
-      <Link className="home__button-link center" to={"./../Congifure"}>
-        <h2>Configure</h2>
-      </Link>
-      <Link className="home__button-link center" to={"./../Logs"}>
-        <h2>Logs</h2>
-      </Link>
+    </div>
+    <div className={"Navigation"}>
+        <Link className="home__button-link" to={"./../Home"}><h2 className="home__button-link"> Home </h2></Link>
+        <Link className="home__button-link" to={"./../Congifure"}><h2 className="home__button-link"> Configure </h2></Link>
+        <Link className="home__button-link" to={"./../Logs"}><h2 className="home__button-link"> Logs </h2></Link>
     </div>
     
     </div>
-  );
+);
 }
 
 export default Home;
