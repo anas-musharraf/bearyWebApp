e6ab6eb5859fc9825412c8cde7613b26be19c5fa
anas-musharraf <anas.musharraf1207@gmail.com>
2020-03-09 11:53:15 -0400

    Changes

---
+++ bearyapp/src/components/configure/Configure.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../styles/Configure.css';
+
+function Configure() {
+  return (
+    <div><p>Configure page</p></div>
+    
+  );
+}
+
+export default Configure;
