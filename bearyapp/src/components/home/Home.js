705fe070a877cb76d1bf66f738b518a121c99cee
anas-musharraf <anas.musharraf1207@gmail.com>
2020-03-10 15:19:10 -0400

    Home page done

--- bearyapp/src/components/home/Home.js
+++ bearyapp/src/components/home/Home.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import '../../styles/Home.css';
 
+import { Button } from 'react-bootstrap';
+
 function Home() {
   return (
     <div>
     <div className={"header"}>
-      <h1>Beary</h1>
+        <Link to={"./../Home"}><h1>Beary</h1></Link>
     </div>
-    <div className={"Navigation"}>
-        <Link className="home__button-link" to={"./../Home"}><h2 className="home__button-link"> Home </h2></Link>
-        <Link className="home__button-link" to={"./../Congifure"}><h2 className="home__button-link"> Configure </h2></Link>
-        <Link className="home__button-link" to={"./../Logs"}><h2 className="home__button-link"> Logs </h2></Link>
+    <div>
+        <Button className={"ConfigButton"} variant="warning"><Link to={"./../Configure"}>Configure</Link></Button>
+        <Button className={"LogButton"} variant="warning"><Link to={"./../Logs"}>Logs</Link></Button>
     </div>
-    
     </div>
 );
 }
