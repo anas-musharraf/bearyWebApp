f0132bb7bbf88089a080ad7c8d9fd71b915eb6e2
Amir Mousavi <psamb75@gmail.com>
2020-03-24 13:54:57 -0400

    Add server code Add Axios code for server side APIs Refactor front end to its own dir

---
+++ bearyapp/bearyFront/src/api/responses.js
@@ -0,0 +1,79 @@
+const domain_url = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"
+
+const global_options = {
+    credentials: 'include',
+    headers: {
+        Accept: "application/json, text/plain",
+        "Content-Type": "application/json"
+    }
+  }
+  
+  // GET all responses
+  export const getResponses = () => {
+    // the URL for the request
+    const url = "/responses"; // replace with heroku instance?
+  
+    const request = new Request(domain_url + url, global_options);
+  
+    return fetch(request)
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          alert("Could not get responses");
+        }
+      })
+      .catch(error => {
+        console.log("failed to get the resources")
+        console.log(error);
+      });
+  };
+
+  // GET request for a specific emotion
+  export const getResponse = (emotion) => {
+    // the URL for the request
+    const url = `/responses/${emotion}`;
+  
+    const request = new Request(domain_url + url, global_options);
+  
+    return fetch(request)
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return Promise.reject("Couldn't find the response");
+        }
+      })
+      .catch(error => {
+        alert(error.message)
+      });
+  };
+
+// POST request to add a new response, it has to be an obj = {emotion:   , text:  }
+export const addResponse = (response) => {
+    // the URL for the request
+    const url = "/responses";
+  
+    // Create our request constructor with all the parameters we need
+    const request = new Request(domain_url + url, {
+      method: "post",
+      body: JSON.stringify(response),
+      credentials: "include",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      }
+    });
+  
+    // Send the request with fetch()
+    return fetch(request)
+      .then(function (res) {
+        return res.json();
+      }).then((json) => {
+        return json;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  
---
+++ bearyapp/server.js
@@ -0,0 +1,114 @@
+"use strict";
+const log = console.log;
+
+const express = require("express");
+const app = express();
+const fs = require('fs');
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+//const session = require("express-session");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/////////// DB operations //////////////////
+const dbPath = "./db/responses.json";
+
+function readDB() {
+    log("reading the db file");
+    var data;
+
+    try {
+        data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+    } catch (e) {
+        data = [];
+    }
+    return data;
+}
+
+function saveToDB(emotion, text) {
+    log("writing the db file");
+    // read the db file
+    const responseObj = readDB();
+
+    for (var i = 0; i < responseObj.length; i++) {
+        const emotionObj = responseObj[i];
+        if (emotionObj.emotion === emotion) {
+            emotionObj.responses.push(text);
+            break;
+        }
+    }
+
+    if (responseObj.length == 0) {
+        responseObj.push(
+            {
+                emotion: emotion,
+                responses: [text]
+            });
+    }
+
+    log(responseObj);
+    // write back to db file
+    try {
+        fs.writeFileSync(dbPath, JSON.stringify(responseObj));
+
+    } catch (e) {
+        log(e)
+    }
+}
+
+/////////////// Routes ////////////////////////
+
+// a GET route to get all responses
+app.get("/responses", (req, res) => {
+    const responses = readDB();
+    res.send( responses);
+});
+
+// a GET route to get responses of a specific emotion
+app.get("/responses/:emotion", (req, res) => {
+    const emotion = req.params.emotion;
+
+    log(emotion)
+
+    const responseObj = readDB();
+    log(responseObj);
+
+    for (var i = 0; i < responseObj.length; i++) {
+        const emotionObj = responseObj[i];
+        if (emotionObj.emotion === emotion) {
+            res.send(emotionObj.responses);
+            return;
+        }
+    }
+    res.status(404).send();
+});
+
+// a POST route to write response to a specific emotion
+app.post("/responses", (req, res) => {
+    const {emotion, text} = req.body;
+    try {
+        saveToDB(emotion, text);
+        res.send("Successfully stored into DB.");
+    } catch(e){
+        res.status(500).send(e);
+    }
+});
+
+/*** Webpage routes below **********************************/
+// Serve the build
+app.use(express.static(__dirname + "/bearyFront/build"));
+
+// All routes other than above will go to index.html
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/bearyFront/build/index.html");
+});
+
+/*************************************************/
+// Express server listening...
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  log(`Listening on port ${port}...`);
+});
